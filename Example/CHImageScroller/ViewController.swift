b4c0028802a26b9478850e7ce4ad5a6213b08a3e
Connor Hailey <connorhailey@gmail.com>
2016-02-25 13:31:45 -0500

    fixed transition, added starting index, made background transparent

--- Example/CHImageScroller/ViewController.swift
+++ Example/CHImageScroller/ViewController.swift
@@ -32,8 +32,8 @@ class ViewController: UIViewController {
     
     func showCHImageScroller(){
         let images = NSArray(objects: UIImage(named: "barca")!, UIImage(named: "bayern")!, UIImage(named: "united")!)
-        
-        let imageScroller = CHImageScroller(images: images, frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
+
+        let imageScroller = CHImageScroller(images: images, startingIndex: 0, frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
         
         self.view.addSubview(imageScroller)
         imageScroller.presentImagePreview()
