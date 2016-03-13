1aab739af17fac2d1a2cc261ee74efac753a2475
Connor Hailey <connorhailey@gmail.com>
2016-03-13 14:19:17 -0400

    Release 0.1.4 - Zoom fix

--- Pod/Classes/CHImageScroller.swift
+++ Pod/Classes/CHImageScroller.swift
@@ -15,14 +15,12 @@ public class CHImageScroller: UIView, UIScrollViewDelegate {
     
     var scrollView: UIScrollView!
     var images: NSArray!
-    var imageViews: NSMutableArray!
     var exitButton: UIButton!
     
     required public init(images: NSArray, startingIndex: Int, frame: CGRect)
     {
         super.init(frame: frame)
         self.images = images
-        self.imageViews = NSMutableArray()
         
         if !UIAccessibilityIsReduceTransparencyEnabled() {
             self.backgroundColor = UIColor.clearColor()
@@ -55,7 +53,6 @@ public class CHImageScroller: UIView, UIScrollViewDelegate {
     func setupScrollView(startingIndex: Int){
         self.scrollView = UIScrollView(frame: CGRectMake(0, self.center.y/2, SCREEN_WIDTH, SCREEN_WIDTH))
         self.scrollView.scrollEnabled = true
-        self.scrollView.maximumZoomScale = 3.0
         self.scrollView.showsHorizontalScrollIndicator = false
         self.scrollView.pagingEnabled = true
         self.scrollView.delegate = self
@@ -70,14 +67,25 @@ public class CHImageScroller: UIView, UIScrollViewDelegate {
     func addImageViews() {
         var imageCount = 0
         for image in self.images {
-            let imageView = UIImageView(frame: CGRectMake(SCREEN_WIDTH*CGFloat(imageCount), 0, SCREEN_WIDTH, self.scrollView.frame.size.height))
+            
+            let imageScrollView = UIScrollView(frame: CGRectMake(SCREEN_WIDTH*CGFloat(imageCount), 0, SCREEN_WIDTH, self.scrollView.frame.size.height))
+            imageScrollView.minimumZoomScale = 1.0
+            imageScrollView.maximumZoomScale = 2.0
+            imageScrollView.zoomScale = 1.0
+            imageScrollView.contentSize = imageScrollView.bounds.size
+            imageScrollView.delegate = self
+            imageScrollView.showsHorizontalScrollIndicator = false
+            imageScrollView.showsVerticalScrollIndicator = false
+
+            let imageView = UIImageView(frame: imageScrollView.bounds)
             imageView.tag = imageCount
             imageView.userInteractionEnabled = true
             imageView.contentMode = UIViewContentMode.ScaleAspectFit
             imageView.image = image as? UIImage
-            self.scrollView.addSubview(imageView)
             
-            self.imageViews.addObject(imageView)
+            imageScrollView.addSubview(imageView)
+
+            self.scrollView.addSubview(imageScrollView)
             self.scrollView.contentSize = CGSizeMake(SCREEN_WIDTH*CGFloat(++imageCount), SCREEN_WIDTH)
         }
     }
@@ -99,8 +107,11 @@ public class CHImageScroller: UIView, UIScrollViewDelegate {
     }
     
     public func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        let imageIndex = scrollView.contentOffset.y/SCREEN_WIDTH
-        return self.imageViews.objectAtIndex(Int(imageIndex)) as? UIView
+        return scrollView.subviews[0]
+    }
+    
+    public func scrollViewDidEndZooming(scrollView: UIScrollView, withView view: UIView?, atScale scale: CGFloat) {
+        
     }
     
     func exit(){
